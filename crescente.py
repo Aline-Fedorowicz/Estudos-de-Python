0ca904402f428f30cfd2614646562c8989771c0c
Aline Fedorowicz <alinewicz@gmail.com>
2019-09-08 20:47:20 -0300

    Add comparação de ordem crescente

---
+++ crescente.py
@@ -0,0 +1,8 @@
+primeiro = int (input("Digite o primeiro número inteiro: "))
+segundo = int (input("Digite o segundo número inteiro: "))
+terceiro = int (input("Digite o terceiro número inteiro: "))
+
+if primeiro < segundo < terceiro:  
+    print("crescente")
+else:
+    print ("Não está em ordem crescente")
