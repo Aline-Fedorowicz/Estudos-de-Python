1183e500dcffbd3c2cd4e41c2beccc16537c3ee7
Aline Fedorowicz <alinewicz@gmail.com>
2019-09-08 20:52:46 -0300

    Add Média Aritmética

---
+++ Média.py
@@ -0,0 +1,8 @@
+primeira = int (input("Digite a primeira nota:"))
+segunda = int (input("Digite a segunda nota:"))
+terceira = int (input("Digite a terceira nota:"))
+quarta = int (input("Digite a quarta nota:"))
+
+Ma = (primeira + segunda + terceira + quarta)/4
+
+print ("A Média Aritimética é:", Ma)
