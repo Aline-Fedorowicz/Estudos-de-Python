f068fb322194de08e6cc11052a913cb3ac1d106b
Aline Fedorowicz <alinewicz@gmail.com>
2019-09-08 20:52:04 -0300

    Add cálculo financeiro

---
+++ Fatura.py
@@ -0,0 +1,8 @@
+
+primeira= input("Digite o nome do cliente:")
+segunda= input ("Digite o dia do vencimento:")
+terceira= input ("Digite o mês do vencimento:")
+quarta= input ("Digite o valor da fatura:")
+
+print ("Olá,", primeira)
+print ("A sua fatura com vencimento em", segunda, "de", terceira, "no valor de R$", quarta, "está fechada.")
