bfc276f7e02b3d3bc65c0fd549c0cd0f6c923fe7
Aline Fedorowicz <alinewicz@gmail.com>
2019-09-08 20:53:54 -0300

    Soma de ints

---
+++ soma.py
@@ -0,0 +1,12 @@
+def main ():
+    a_str = input("Digite o seu primeiro número:")
+    b_str = input("Digite o seu segundo número:")
+
+    a_int= int(a_str)
+    b_int= int(b_str)
+
+    soma= a_int + b_int
+
+    print("A soma dos números", a_int, "+", b_int, "é igual a:", soma )
+
+main()
